5e8607003e5f1ac3275a6e81f801d85afb9720c6
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2022-11-15 15:54:42 -0800

    Search Button Done..

--- src/Country/Country.js
+++ src/Country/Country.js
@@ -16,13 +16,30 @@ function Country() {
         setCountries(newCountries);
     }
 
+    //Search box working:
+    const [search, setSearch] = useState('');
+    
+    const handleOnChange = (event) => {
+        setSearch(event.target.value);
+    }
+
+useEffect(() => {   
+    const results = countries.filter(country => country.name.common.toLowerCase().includes(search.toLowerCase()));
+    setCountries(results);
+// eslint-disable-next-line react-hooks/exhaustive-deps
+}, [ search]);
+
+  
+
     return (
         <>
+        <div className="search-box">
+            <input type="text" onChange={handleOnChange} value={search} placeholder="Search Country" />
+        </div>
             {
                 countries.map(country => (
 
                     <>
-                        
                             <div className="col-lg-4 mycard">
                                 <h3 key={country.name.common}>{country.name.common}</h3>
                                 <p >Capital: {country.capital}</p>
